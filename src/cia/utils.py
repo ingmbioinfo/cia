8fbbe3552b4681e9db1d18891830481e39848198
Ivan Ferrari <72440462+ivanferrari95@users.noreply.github.com>
2024-03-12 14:42:23 +0100

    Update utils.py

--- src/cia/utils.py
+++ src/cia/utils.py
@@ -4,202 +4,132 @@ import itertools
 import scipy
 
 def signatures_similarity(signatures_dict, show='J'):
-    
-    """ 
-    A function that computes the similary between gene signatures.
-           
+    """
+    Computes the similarity between gene signatures.
+
     Parameters
     ----------
-     
-    
-    signatures_dict: dict
-        a dictionary having as keys the signature names and as values the gene signatures (lists of gene names).
-        
-    show: str
-        a string that specifies if similarities will be returned as Jaccard index ("J") or as percentages of intersection ("%").
-    
+    signatures_dict : dict
+        A dictionary having as keys the signature names and as values the lists of gene names (gene signatures).
+    show : str, optional
+        Specifies the metric for showing similarities: 'J' for Jaccard index or '%' for percentages of intersection.
+        Default is 'J'.
+
     Returns
     -------
-    
-    similarity: pandas.DataFrame
-        a pandas.DataFame containing the similarity of each pair of signatures.
-        
+    similarity : pandas.DataFrame
+        A DataFrame containing the similarity of each pair of signatures, with signatures as both rows and columns.
+
     Raises
     ------
-    
     ValueError
-        if show is different from "J" or "%".
+        If 'show' is different from 'J' or '%'.
+
+    Example
+    -------
+    >>> signatures = {
+    >>>     'signature1': ['gene1', 'gene2', 'gene3'],
+    >>>     'signature2': ['gene2', 'gene3', 'gene4'],
+    >>>     'signature3': ['gene1', 'gene5']
+    >>> }
+    >>> similarity = signatures_similarity(signatures, show='J')
+    >>> print(similarity)
     """
+    if show not in ['J', '%']:
+        raise ValueError('show must be "J" or "%".')
+    
+    signature_names = list(signatures_dict.keys())
+    n = len(signature_names)
+    similarity_matrix = np.zeros((n, n))
 
-
-    similarity={}
-    for i in signatures_dict.keys():
-        values=[]
-        if show=='J':
-            for j in signatures_dict.keys():
-                values.append(len(np.intersect1d(signatures_dict[i], signatures_dict[j]))/len(np.union1d(signatures_dict[i], signatures_dict[j])))
-        elif show=='%':
-            for j in signatures_dict.keys():
-                values.append(round(100*len(np.intersect1d(signatures_dict[i], signatures_dict[j]))/len(signatures_dict[i]),2))
+    for i in range(n):
+        for j in range(i, n):
+            intersec = len(np.intersect1d(signatures_dict[signature_names[i]], signatures_dict[signature_names[j]]))
+            if show == 'J':
+                union = len(np.union1d(signatures_dict[signature_names[i]], signatures_dict[signature_names[j]]))
+                similarity = intersec / union
+            elif show == '%':
+                similarity = round(100 * intersec / len(signatures_dict[signature_names[i]]), 2)
             
-        else:
-            raise ValueError('show must be "J" or "%".')
-        similarity[i]=values
-    similarity=pd.DataFrame(similarity)
-    similarity.index= similarity.columns
+            similarity_matrix[i, j] = similarity_matrix[j, i] = similarity
+
+    similarity = pd.DataFrame(similarity_matrix, index=signature_names, columns=signature_names)
     return similarity
 
+import pandas as pd
+import numpy as np
+import scipy.sparse
 
-def filter_degs(data, groupby, uns_key='rank_genes_groups', direction='up', logFC=0,  scores=None, perc= 0, mean=0):
+def filter_degs(data, groupby, uns_key='rank_genes_groups', direction='up', logFC=0, scores=None, perc=0, mean=0):
+    """
+    Filters differentially expressed genes (DEGs) obtained with scanpy.tl.rank_genes_groups based on given thresholds.
     
-    """ 
-    A function that filters differentially expressed genes (DEGs) obtaied with scanpy.tl.rank_genes_groups.
-           
     Parameters
     ----------
-
-    data: anndata.AnnData
-        an AnnData object.     
-    
-    groupby: str
-        a string that specifies the AnnData.obs column containing cell groups of the differential expression analysis.
-
-    uns_key: str
-        a string that specifies the AnnData.uns where differential expression analysis results are stored.
-    
-    direction: str
-        a string that specifies if filtering-in above thersholds ("up", to select upregulated genes) or below thresholds ("down", to select down regulated genes).
-    
-    logFC: int or float
-        Log fold-change threshold.
-
-    scores: int, float or None
-        Z score threshold.
-    
-    perc: int or float
-        Threshold of the percentage of expressing cell within the cell group.
-    
-    mean: int or float
-        Mean expression threshold.
-        
+    data : anndata.AnnData
+        An AnnData object containing the analysis results.
+    groupby : str
+        Column in AnnData.obs containing cell group labels.
+    uns_key : str
+        Key in AnnData.uns where differential expression analysis results are stored.
+    direction : str
+        Specifies if filtering for upregulated ('up') or downregulated ('down') genes.
+    logFC : float
+        Log fold change threshold to filter genes.
+    scores : float, optional
+        Z score threshold to filter genes.
+    perc : float
+        Percentage of cells expressing the gene threshold.
+    mean : float
+        Mean expression threshold to filter genes.
     
     Returns
     -------
-         
-    signatures_dict: dict
-        a dictionary having as keys the cell group names and as values the filtered-in DEGs  (lists of gene names).
-        
+    signatures_dict : dict
+        Dictionary with cell group names as keys and lists of filtered gene names as values.
+    
     Raises
     ------
-    
     ValueError
-        if direction is different from "up" or "down".
-    """
-
-    signatures_dict={}
-    for group in data.obs[groupby].cat.categories:
-    #for group in data.uns[uns_key]['names'].dtype.names:
-        degs=data.uns[uns_key]['names'][group] 
-
-
-        n_cells=sum(data.obs[groupby]==group)
-
-
-        
-        if direction=='up':
-            
-            order=pd.DataFrame(data.uns[uns_key]['logfoldchanges'][group]).sort_values(by=0,ascending=False).index
-            degs= degs[order]
-
-            if scipy.sparse.issparse(data.raw.X):
-                cells = (np.array(data.raw[data.obs[groupby].isin([group])][:,degs.tolist()].X.todense() > 0).sum(axis=0)/n_cells*100)
-            else:
-                cells = (np.array(data.raw[data.obs[groupby].isin([group])][:,degs.tolist()].X > 0).sum(axis=0)/n_cells*100)
-            cells =(cells >= perc)
-            
-            gene_mean = np.ravel(data.raw[data.obs[groupby].isin([group])][:,degs.tolist()].X.mean(0))
-            gene_mean = (gene_mean >= mean )
-            
-            lfc= data.uns[uns_key]['logfoldchanges'][group]
-            lfc= (lfc[order] >=logFC)
-            
-            filters=[cells, gene_mean, lfc]
-            
-            if scores!=None:
-                s= data.uns[uns_key]['scores'][group]
-                s= (s[order] >=scores)
-                filters.append(s)
-            
-
-            filters=np.bitwise_and.reduce(filters)
-            signatures_dict[group]= degs[filters].tolist()
-            
-            
-
-            
-        elif direction=='down':
-
-            order=pd.DataFrame(data.uns[uns_key]['logfoldchanges'][group]).sort_values(by=0,ascending=False).index
-            degs= degs[order]
-            
-            if scipy.sparse.issparse(data.raw.X):
-                cells = (np.array(data.raw[data.obs[groupby].isin([group])][:,degs.tolist()].X.todense() > 0).sum(axis=0)/n_cells*100)
-            else:
-                cells = (np.array(data.raw[data.obs[groupby].isin([group])][:,degs.tolist()].X> 0).sum(axis=0)/n_cells*100)
-            cells =(cells <= perc)
-            
-            gene_mean = np.ravel(data.raw[data.obs[groupby].isin([group])][:,degs.tolist()].X.mean(0))
-            gene_mean = (gene_mean <= mean )
-            
-            lfc= data.uns[uns_key]['logfoldchanges'][group]
-            lfc= (lfc[order] <=logFC)
-            
-            filters=[cells, gene_mean, lfc]
-            
-            if scores!=None:
-                s= data.uns[uns_key]['scores'][group]
-                s= (s[order] <=scores)
-                filters.append(s)
-            
-
-            filters=np.bitwise_and.reduce(filters)
-            signatures_dict[group]= degs[filters].tolist()
-            
-        else:
-            raise ValueError('direction must be "up" or "down".')
-        
-    return signatures_dict
-
-def read_gmt(file, sep='\t', header=None):
-    """
-    A function to convert a gmt file (with or without description of signatures) into a dictionary.
+        If 'direction' is not 'up' or 'down'.
     
-    Parameters
-    ----------
-     
-    file: str
-        filepath of gmt file. See pandas.read_csv documentation.   
-    sep: str
-        delimiter to use. See pandas.read_csv documentation.
-    header: int, list(ind) or None
-        row number(s) to use as the column names, and the start of the data. See pandas.read_csv documentation. 
-
-    Returns
+    Example
     -------
-    
-    gmt: dict
-        a dictionary having the signature names as keys and gene lists as values.
+    >>> import scanpy as sc
+    >>> adata = sc.datasets.pbmc68k_reduced()
+    >>> sc.tl.rank_genes_groups(adata, 'louvain', method='t-test')
+    >>> filtered_genes = filter_degs(adata, 'louvain', direction='up', logFC=1, perc=10, mean=0.1)
+    >>> print(filtered_genes['0'])  # Show filtered genes for the first group
     """
-    gmt={}
-    df=pd.read_csv(file, sep=sep, header=header)
-    if sum([len(i.split())>1 for i in df.iloc[:,1]])>0:
-        for i in range(0, len(df.index)):
-            gmt[df.iloc[i,0]]=list(df.iloc[i,2:].dropna())
-            
-    else:
-        for i in range(0, len(df.index)):
-            gmt[df.iloc[i,0]]=list(df.iloc[i,1:].dropna())
-    return gmt
+    if direction not in ['up', 'down']:
+        raise ValueError('direction must be "up" or "down".')
+
+    signatures_dict = {}
+    for group in data.obs[groupby].cat.categories:
+        degs = data.uns[uns_key]['names'][group]
+        lfc = data.uns[uns_key]['logfoldchanges'][group]
+        cells_expr = data.raw[:, degs].X
+
+        if scipy.sparse.issparse(cells_expr):
+            cells_expr = cells_expr.todense()
+
+        n_cells = sum(data.obs[groupby] == group)
+        cells_percentage = (np.array(cells_expr > 0).sum(axis=0) / n_cells * 100).flatten()
+        gene_mean = cells_expr.mean(axis=0).flatten()
+
+        if direction == 'up':
+            filter_cond = (lfc >= logFC) & (cells_percentage >= perc) & (gene_mean >= mean)
+        else:  # 'down'
+            filter_cond = (lfc <= logFC) & (cells_percentage <= perc) & (gene_mean <= mean)
+
+        if scores is not None:
+            gene_scores = data.uns[uns_key]['scores'][group]
+            filter_cond &= (gene_scores >= scores) if direction == 'up' else (gene_scores <= scores)
+
+        filtered_genes = degs[filter_cond]
+        signatures_dict[group] = filtered_genes.tolist()
+
+    return signatures_dict
 
 def save_gmt(signatures_dict, file):
     """
@@ -214,3 +144,29 @@ def save_gmt(signatures_dict, file):
         filepath of gmt file. See pandas.DataFrame.to_csv documentation.  
     """
     pd.DataFrame.from_dict(signatures_dict, orient='index').to_csv(file, sep='\t', header=None)
+import pandas as pd
+
+def save_gmt(signatures_dict, file_path):
+    """
+    Saves a dictionary of gene signatures to a file in GMT format, suitable for use with 
+    signature scoring and classification functions.
+
+    Parameters
+    ----------
+    signatures_dict : dict
+        A dictionary with signature names as keys and lists of gene names as values.
+    file_path : str
+        The file path where the GMT file will be saved. The file will be saved in tab-separated format.
+
+    Example
+    -------
+    >>> signatures_dict = {
+    >>>     'signature1': ['gene1', 'gene2', 'gene3'],
+    >>>     'signature2': ['gene4', 'gene5', 'gene6']
+    >>> }
+    >>> save_gmt(signatures_dict, 'signatures.gmt')
+
+    This will save the signatures in 'signatures.gmt' in GMT format.
+    """
+    # Transform the dictionary into a DataFrame and save it in GMT format
+    pd.DataFrame.from_dict(signatures_dict, orient='index').to_csv(file_path, sep='\t', header=False)

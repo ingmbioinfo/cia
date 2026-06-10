b7d95572a01f0249063a84f1edde63f14359f3d1
AndGob <gobbini@ingm.org>
2026-06-10 13:19:34 +0000

    bugfix: Restored docstring for funciton already implemented in the main utils.py

--- src/cia/utils.py
+++ src/cia/utils.py
@@ -9,6 +9,24 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
 def _fetch_url_gmt(url: str) -> dict:
+    """Fetches a GMT file from a URL and parses it into a dictionary.
+
+    Parameters
+    ----------
+    url : str
+        The URL of the GMT file.
+
+    Returns
+    -------
+    dict
+        A dictionary where keys are gene set names and values are lists of associated genes.
+
+    Raises
+    ------
+    ValueError
+        If the request to fetch the GMT file fails.
+    """
+
     response = requests.get(url)
     if response.status_code != 200:
         raise ValueError(
@@ -30,6 +48,38 @@ def _fetch_url_gmt(url: str) -> dict:
 
 
 def signatures_similarity(signatures_dict, show="J"):
+    """
+    Computes the similarity between gene signatures.
+
+    Parameters
+    ----------
+    signatures_dict : dict
+        A dictionary having as keys the signature names and as values the lists of gene names (gene signatures).
+    show : str, optional
+        Specifies the metric for showing similarities: 'J' for Jaccard index or '%' for percentages of intersection.
+        Default is 'J'.
+
+    Returns
+    -------
+    similarity : pandas.DataFrame
+        A DataFrame containing the similarity of each pair of signatures, with signatures as both rows and columns.
+
+    Raises
+    ------
+    ValueError
+        If 'show' is different from 'J' or '%'.
+
+    Example
+    -------
+    >>> signatures = {
+    >>>     'signature1': ['gene1', 'gene2', 'gene3'],
+    >>>     'signature2': ['gene2', 'gene3', 'gene4'],
+    >>>     'signature3': ['gene1', 'gene5']
+    >>> }
+    >>> similarity = signatures_similarity(signatures, show='J')
+    >>> print(similarity)
+    """
+
     if show not in ["J", "%"]:
         raise ValueError('show must be "J" or "%".')
 
@@ -75,6 +125,47 @@ def filter_degs(
     perc=0,
     mean=0,
 ):
+    """
+    Filters differentially expressed genes (DEGs) obtained with scanpy.tl.rank_genes_groups based on given thresholds.
+    
+    Parameters
+    ----------
+    data : anndata.AnnData
+        An AnnData object containing the analysis results.
+    groupby : str
+        Column in AnnData.obs containing cell group labels.
+    uns_key : str
+        Key in AnnData.uns where differential expression analysis results are stored.
+    direction : str
+        Specifies if filtering for upregulated ('up') or downregulated ('down') genes.
+    logFC : float
+        Log fold change threshold to filter genes.
+    scores : float, optional
+        Z score threshold to filter genes.
+    perc : float
+        Percentage of cells expressing the gene threshold.
+    mean : float
+        Mean expression threshold to filter genes.
+    
+    Returns
+    -------
+    signatures_dict : dict
+        Dictionary with cell group names as keys and lists of filtered gene names as values.
+    
+    Raises
+    ------
+    ValueError
+        If 'direction' is not 'up' or 'down'.
+    
+    Example
+    -------
+    >>> import scanpy as sc
+    >>> adata = sc.datasets.pbmc68k_reduced()
+    >>> sc.tl.rank_genes_groups(adata, 'louvain', method='t-test')
+    >>> filtered_genes = filter_degs(adata, 'louvain', direction='up', logFC=1, perc=10, mean=0.1)
+    >>> print(filtered_genes['0'])  # Show filtered genes for the first group
+    """
+
     signatures_dict = {}
 
     for group in data.obs[groupby].cat.categories:
@@ -537,6 +628,18 @@ def retrieve_optimal_markers(
 
 
 def save_gmt(signatures_dict, file):
+        """
+    A function to convert a dictionary of signatures in a gmt file correctly formatted for signature_score and signature_based_classification functions.
+    
+    Parameters
+    ----------
+     
+    signatures_dict: dict
+        a dictionary having as keys the signature names and as values the gene signatures (lists of gene names).
+    file: str
+        filepath of gmt file. See pandas.DataFrame.to_csv documentation.  
+    """
+    
     with open(file, "w") as f:
         for key, values in signatures_dict.items():
             line = key + "\t" + key + "\t" + "\t".join(values) + "\n"
